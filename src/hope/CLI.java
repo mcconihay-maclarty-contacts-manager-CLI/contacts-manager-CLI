1eb43ad1d4a2e2f8e0d99e71128355a628949836
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-13 18:29:06 -0600

    deleteContact mk3

--- src/hope/CLI.java
+++ src/hope/CLI.java
@@ -17,6 +17,12 @@ public class CLI {
 	//		Combine them
 	Path dataFile = Paths.get(directory, filename);
 	Path dataTempFile = Paths.get(directory, fileTemp);
+//BufferedWriter is a subclass to writer
+//	BufferedWriter writes text to character output stream, buffering characters so as to provide for the efficient writing of single characters, arrays, and strings.
+
+// readline() reads a text line that ends with \n
+
+//Java FileWriter class is used to write character-oriented data to a file. Unlike FileOutputStream class, you don't need to convert string into byte array because it provides method to write string directly.
 
 
 //	allContacts uses a reader to use a relative path to the text file contactInfo. Once in the file
@@ -38,7 +44,7 @@ public class CLI {
 
 
 //addContact takes in two separate strings so the user has to enter both in separate scanners.
-//	the FileWriter is using a relative path anf is set to true, allowing Filewriter to append instead of overwriting.
+//	the FileWriter is using a relative path and is set to true, allowing Filewriter to append instead of overwriting.
 //	newLine causes the next writer to write on a new line
 //	when using a writer you have to use .close inorder for the function to finish
 	public void addContact(String name, String number) {
@@ -61,7 +67,6 @@ public class CLI {
 //	Maybe delete by index and add a number that increments
 	public void deleteContact(String lineToRemove) throws IOException {
 
-
 		File inputFile = new File("./src/data/contactInfo.text");
 		File tempFile = new File("./src/data/myTempFile.txt");
 
@@ -71,15 +76,15 @@ public class CLI {
 		String Line;
 
 		while((Line = reader.readLine()) != null) {
-			if(!Line.toLowerCase(Locale.ROOT).contains(lineToRemove.toLowerCase(Locale.ROOT))){
+			if(!Line.toLowerCase().contains(lineToRemove.toLowerCase())){
 //				what is locale, ROOT,
 				writer.write(Line + System.getProperty("line.separator"));
 //				getProp and line.separator
+				}
 			}
-		}
-		writer.close();
-		reader.close();
-		boolean successful = tempFile.renameTo(inputFile);
+			writer.close();
+			reader.close();
+			boolean successful = tempFile.renameTo(inputFile);
 	}
 
 
@@ -91,7 +96,7 @@ public class CLI {
 			BufferedReader reader = new BufferedReader(new FileReader("./src/data/contactInfo.text"));
 			String line;
 			while((line = reader.readLine()) != null){
-				if(line.toLowerCase(Locale.ROOT).contains(name.toLowerCase(Locale.ROOT))) {
+				if(line.toLowerCase().contains(name.toLowerCase())) {
 					System.out.println(line);
 				}
 			}
@@ -102,7 +107,4 @@ public class CLI {
 
 }
 
-//---Name---|----Number----|
-//		Bob Vance | 909-090-9090 |
-//		Lost Boys | 909-999-0000 |
-//		Joe Wallace | 909-505-3939 |
+
